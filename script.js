8679ad234e0b9ed2285a035a7490b281dbbc8834
1707akash <1707akash@gmail.com>
2024-01-14 00:08:43 +0530

    styling edited

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("hii");
+
+
+let bodyBg= document.getElementById("dayBody");
+let toggleDiv = document.getElementById("toggleDiv");
+let circleDay = document.getElementById("circleDay");
+let heading = document.getElementById("dayHeading");
+
+
+
+let j=0;
+toggleDiv.addEventListener("click",function(){
+    j++;
+    if(j%2===1){
+        bodyBg.id = "nightBody";
+        circleDay.id = "circleNight";
+        heading.id = "nightHeading";
+    }
+    else{
+        bodyBg.id = "dayBody";
+        circleDay.id = "circleDay";
+        heading.id = "dayHeading";
+    }
+    
+});
+
+
